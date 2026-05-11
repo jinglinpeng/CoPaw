238e104b3a7ecbf159fe844bd7821fa2c6d25afe
Jinglin Peng <jinglinpeng@gmail.com>
2026-05-11 11:47:14 +0800

    fix: address desktop codecheck issues

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -180,9 +180,7 @@ export default defineConfig(({ mode }) => {
       },
       // Tauri uses Chromium on Windows and WebKit on macOS and Linux
       target:
-        process.env.TAURI_ENV_PLATFORM === "windows"
-          ? "chrome105"
-          : "safari15",
+        process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari15",
       // don't minify for debug builds
       minify: mode === "production" ? "esbuild" : false,
     },
